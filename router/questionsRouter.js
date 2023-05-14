2e38fc8e8cd7cc1500f06ccbe3dc8090bfa99d1f
Pochoutas <JenniferMonikaStabenau@outlook.fr>
2023-05-14 10:53:54 +0200

    add route delete and updateget

--- router/questionsRouter.js
+++ router/questionsRouter.js
@@ -2,24 +2,61 @@ const questionModel = require('../models/questionModel');
 
 const questionRouter = require('express').Router();
 
-questionRouter.get('/', async (req,res)=>{
-try {
-    res.render('pages/addQuestion.twig')
-} catch (error) {
-    console.log(error);
-    res.send(error)
-}
-})
-
-questionRouter.post('/addquestion', async (req,res)=>{
+questionRouter.get('/', async (req, res) => {
     try {
-     let question = new questionModel(req.body)
-     await question.save()
-     res.redirect('/')
+        res.render('pages/addQuestion.twig')
     } catch (error) {
         console.log(error);
         res.send(error)
     }
-    })
+})
+
+questionRouter.get('/displayquestion', async (req, res) => {
+    try {
+        let questions = await questionModel.find()
+        res.render('pages/diplayquestion.twig', {
+            questions: questions
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.send(error)
+    }
+})
+
+questionRouter.get('/deletequestion/:id', async (req, res) => {
+    try {
+        let question = await questionModel.deleteOne({_id : req.params.id})
+        res.redirect('/displayquestion')
+
+    } catch (error) {
+        console.log(error);
+        res.send(error)
+    }
+})
+
+questionRouter.get('/getupdatequestion/:id', async (req, res) => {
+    try {
+        let question = await questionModel.findOne({_id : req.params.id})
+        res.render('pages/addQuestion.twig', {
+            question: question
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.send(error)
+    }
+})
+
+questionRouter.post('/addquestion', async (req, res) => {
+    try {
+        let question = new questionModel(req.body)
+        await question.save()
+        res.redirect('/')
+    } catch (error) {
+        console.log(error);
+        res.send(error)
+    }
+})
 
 module.exports = questionRouter;
